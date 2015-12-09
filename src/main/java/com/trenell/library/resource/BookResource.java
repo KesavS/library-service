c87f883ed3f2c2ce263f4dcb5e275e71484cd459
Trenell Galman <trenellgalman@gmail.com>
2015-12-09 21:47:11 +0000

    Added first method to the service interface

--- src/main/java/com/trenell/library/resource/BookResource.java
+++ src/main/java/com/trenell/library/resource/BookResource.java
@@ -1,4 +1,17 @@
 package com.trenell.library.resource;
 
+import com.trenell.library.model.Book;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/books")
 public interface BookResource {
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Book create(Book book);
 }
