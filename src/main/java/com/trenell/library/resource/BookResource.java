dd6b6c1dbff6ec525774f092d274735f495b6748
Trenell Galman <trenellgalman@gmail.com>
2015-12-10 15:38:55 +0000

    Added all of the interfaces for the library service.

--- src/main/java/com/trenell/library/resource/BookResource.java
+++ src/main/java/com/trenell/library/resource/BookResource.java
@@ -2,16 +2,38 @@ package com.trenell.library.resource;
 
 import com.trenell.library.model.Book;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/books")
 public interface BookResource {
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<Book> getBooks();
+
+  @GET
+  @PathParam("{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Book findById(@PathParam("id") String id);
+
+  @GET
+  @PathParam("/search/{isbn}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Book findByISBN(@PathParam("isbn") String isbn);
+
+  @PUT
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Book update(Book book);
+
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public Book create(Book book);
+
+  @DELETE
+  @Path("{id}")
+  public void delete(@PathParam("id") String id);
 }
