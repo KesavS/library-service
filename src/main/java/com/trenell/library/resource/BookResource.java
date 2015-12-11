b62e42ec2fe41a5427cd19e7735bc2ebdf7bb41e
Trenell Galman <trenellgalman@gmail.com>
2015-12-11 16:46:36 -0500

    Changed the test logback.xml from debug to info and added methods to the BookResource interface

--- src/main/java/com/trenell/library/resource/BookResource.java
+++ src/main/java/com/trenell/library/resource/BookResource.java
@@ -2,16 +2,29 @@ package com.trenell.library.resource;
 
 import com.trenell.library.model.Book;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/books")
 public interface BookResource {
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<Book> findAll();
+
+  @GET
+  @Path("{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Book findById(@PathParam("id") int id);
+
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public Book create(Book book);
+
+  @PUT
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Book update(Book book);
 }
